e6ceb2fc8f7cc227268b7b2f83c3cc56120860b5
radu mc <radu.michc@gmail.com>
2021-02-15 23:41:38 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("See?");
+           
+        }
+    }
+}
